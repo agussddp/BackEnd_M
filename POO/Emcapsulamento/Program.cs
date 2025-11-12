d3c23ef912b16e124a04c15d7ff0dfc0d18dc4ad
GIULIA MARZANO MARTINIANO DOS SANTOS <54781547842@info.local>
2025-11-12 13:39:23 -0300

    Aula Invertida

---
+++ POO/Emcapsulamento/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using Emcapsulamento;
+
+Console.Clear();
+Pessoa p1 = new Pessoa("Fulano", 85, 1.88);
+
+p1.ExibirDados();
+
+
--- POO/PilaresPOO/Heranca/obj/Debug/net9.0/Heranca.AssemblyInfo.cs
+++ POO/PilaresPOO/Heranca/obj/Debug/net9.0/Heranca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc4597c194571a5cc0f0e3b198e141cb02fc11d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4257aafff43706870066f09ded379414d3d6fd32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
